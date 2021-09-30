7d2b869452fded21cbb10b6dfad2c164167fb6cf
nstancoff <91293577+nstancoff@users.noreply.github.com>
2021-09-30 14:06:24 -0400

    Update Unit_Conversion_Practice_v1.java

--- Unit_Conversion_Practice_v1.java
+++ Unit_Conversion_Practice_v1.java
@@ -3,23 +3,23 @@ public class Unit_Conversion_Practice_v1 {
     public static void main(String[] args) {
         Unit_Conversion_v1_Reference ref = new Unit_Conversion_v1_Reference();
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter original unit (in, ft, m, cm, mm): ");
-        String unit_in = in.nextLine();
-        System.out.println("Enter conversion unit (cannot be the same as the original): ");
-        String unit_out = in.next();
-        System.out.println("Enter the amount: ");
-        double x = in.nextDouble();
-        boolean in_foot = unit_in.equalsIgnoreCase("ft");
-        boolean in_inch = unit_in.equalsIgnoreCase("in");
-        boolean in_meter = unit_in.equalsIgnoreCase("m");
-        boolean in_centi = unit_in.equalsIgnoreCase("cm");
-        boolean in_milli = unit_in.equalsIgnoreCase("mm");
-        boolean out_foot = unit_out.equalsIgnoreCase("ft");
-        boolean out_inch = unit_out.equalsIgnoreCase("in");
-        boolean out_meter = unit_out.equalsIgnoreCase("m");
-        boolean out_centi = unit_out.equalsIgnoreCase("cm");
-        boolean out_milli = unit_out.equalsIgnoreCase("mm");
         while (true) {
+            System.out.println("Enter original unit (in, ft, m, cm, mm): ");
+            String unit_in = in.next();
+            System.out.println("Enter conversion unit (cannot be the same as the original): ");
+            String unit_out = in.next();
+            System.out.println("Enter the amount: ");
+            double x = in.nextDouble();
+            boolean in_foot = unit_in.equalsIgnoreCase("ft");
+            boolean in_inch = unit_in.equalsIgnoreCase("in");
+            boolean in_meter = unit_in.equalsIgnoreCase("m");
+            boolean in_centi = unit_in.equalsIgnoreCase("cm");
+            boolean in_milli = unit_in.equalsIgnoreCase("mm");
+            boolean out_foot = unit_out.equalsIgnoreCase("ft");
+            boolean out_inch = unit_out.equalsIgnoreCase("in");
+            boolean out_meter = unit_out.equalsIgnoreCase("m");
+            boolean out_centi = unit_out.equalsIgnoreCase("cm");
+            boolean out_milli = unit_out.equalsIgnoreCase("mm");
             if (in_foot) {
                 if (out_inch) {
                     System.out.println(ref.ft_to_in(x));
